fc35a71c3665353f8ea6513858a4ff9d1166a92c
JJisak <76518907+JJisak@users.noreply.github.com>
2023-05-14 01:54:28 -0400

    Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,33 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         Assignment8 assignment8 = new Assignment8();
-        List<Integer> numbers = assignment8.getNumbers();
 
-        Map<Integer, Integer> countMap = new HashMap<>();
-        for (int number : numbers) {
-            countMap.put(number, countMap.getOrDefault(number, 0) + 1);
-        }
+        // Use CompletableFuture to asynchronously fetch the data
+        CompletableFuture<List<Integer>> completableFuture = CompletableFuture.supplyAsync(() -> {
+            List<Integer> numbers = new ArrayList<>();
+            while (numbers.size() < 1000000) {
+                numbers.addAll(assignment8.getNumbers());
+            }
+            return numbers;
+        });
 
-        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
-            System.out.println(entry.getKey() + " = " + entry.getValue() );
-        }
+        // Wait for the data to be fetched
+        List<Integer> numbers = completableFuture.get();
+
+        // Count the number of occurrences of each unique number
+        Map<Integer, Integer> counts = new ConcurrentHashMap<>();
+        numbers.parallelStream().forEach(n -> {
+            counts.compute(n, (k, v) -> v == null ? 1 : v + 1);
+        });
+
+        // Print out the count of each unique number
+        counts.forEach((k, v) -> System.out.println(k + " = " + v ));
     }
 }
