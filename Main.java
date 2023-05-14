4244a668b3020402aa9ee4012b50c1896f2ddb5e
JJisak <76518907+JJisak@users.noreply.github.com>
2023-05-14 01:54:55 -0400

    Main.java

--- Main.java
+++ Main.java
@@ -18,16 +18,12 @@ public class Main {
             return numbers;
         });
 
-        // Wait for the data to be fetched
         List<Integer> numbers = completableFuture.get();
 
-        // Count the number of occurrences of each unique number
         Map<Integer, Integer> counts = new ConcurrentHashMap<>();
         numbers.parallelStream().forEach(n -> {
             counts.compute(n, (k, v) -> v == null ? 1 : v + 1);
         });
-
-        // Print out the count of each unique number
         counts.forEach((k, v) -> System.out.println(k + " = " + v ));
     }
 }
