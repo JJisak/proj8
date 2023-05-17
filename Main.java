3c277cc3b65ce312a85c3c2be83a4fcdbb1d1d87
JJisak <76518907+JJisak@users.noreply.github.com>
2023-05-17 10:15:20 -0400

    Main.java

--- Main.java
+++ Main.java
@@ -1,29 +1,26 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Main {
-
-    public static void main(String[] args) {
-
-        // Get the numbers from the Assignment8 class asynchronously
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         Assignment8 assignment8 = new Assignment8();
-        CompletableFuture<List<List<Integer>>> future = CompletableFuture.supplyAsync(assignment8::getAllNumbers);
-
-        // Combine all batches of numbers and count the occurrences of each number
-        Map<Integer, Long> countMap = future.thenApplyAsync(batches -> batches.stream()
-                        .flatMap(List::stream)
-                        .collect(Collectors.groupingBy(Integer::intValue, Collectors.counting())))
-                .join();
-
-        // Print the number of times each unique number appears
-        for (Map.Entry<Integer, Long> entry : countMap.entrySet()) {
-            System.out.println(entry.getKey() + " = " + entry.getValue() );
+        List<CompletableFuture<List<Integer>>> futures = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            futures.add(CompletableFuture.supplyAsync(() -> assignment8.getNumbers()));
         }
-    }
 
+        CompletableFuture<List<Integer>> allFutures = CompletableFuture.allOf(
+                        futures.toArray(new CompletableFuture[futures.size()]))
+                .thenApply(v -> futures.stream()
+                        .map(CompletableFuture::join)
+                        .flatMap(List::stream)
+                        .collect(Collectors.toList()));
+
+        Map<Integer, Long> counts = allFutures.get().stream()
+                .collect(Collectors.groupingBy(Integer::intValue, Collectors.counting()));
+
+        System.out.println(counts);
+    }
 }
