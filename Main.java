004634c4e3033ea83204871a750c4af0b58aadf2
JJisak <76518907+JJisak@users.noreply.github.com>
2023-05-20 16:03:04 -0400

    Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,36 @@
-import java.util.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         Assignment8 assignment8 = new Assignment8();
         List<CompletableFuture<List<Integer>>> futures = new ArrayList<>();
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
+
         for (int i = 0; i < 1000; i++) {
-            futures.add(CompletableFuture.supplyAsync(() -> assignment8.getNumbers()));
+            int finalI = i;
+            CompletableFuture<List<Integer>> future = CompletableFuture.supplyAsync(() -> assignment8.getNumbers(finalI), executorService);
+            futures.add(future);
         }
 
-        CompletableFuture<List<Integer>> allFutures = CompletableFuture.allOf(
-                        futures.toArray(new CompletableFuture[futures.size()]))
-                .thenApply(v -> futures.stream()
-                        .map(CompletableFuture::join)
-                        .flatMap(List::stream)
+        CompletableFuture<Void> allFutures = CompletableFuture.allOf(
+                futures.toArray(new CompletableFuture[0]));
+
+        CompletableFuture<List<Integer>> combinedFuture = allFutures.thenApply(v ->
+                futures.stream()
+                        .flatMap(future -> future.join().stream())
                         .collect(Collectors.toList()));
 
-        Map<Integer, Long> counts = allFutures.get().stream()
+        Map<Integer, Long> counts = combinedFuture.get().stream()
                 .collect(Collectors.groupingBy(Integer::intValue, Collectors.counting()));
 
         System.out.println(counts);
+
+        executorService.shutdown();
     }
 }
+
